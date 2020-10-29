0cfadc50a4eb57d2aebb60d6fd9008032c5466bc
Moustafa-nour <70019395+Moustafa-nour@users.noreply.github.com>
2020-10-29 14:04:41 +0200

    project code solution

--- projects/P04-eCommerce Application/starter_code/src/main/java/com/example/demo/model/persistence/User.java
+++ projects/P04-eCommerce Application/starter_code/src/main/java/com/example/demo/model/persistence/User.java
@@ -1,17 +1,10 @@
 package com.example.demo.model.persistence;
 
-import javax.persistence.CascadeType;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.OneToOne;
-import javax.persistence.Table;
-
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonProperty.Access;
+
+import javax.persistence.*;
 
 
 @Entity
@@ -26,7 +19,37 @@ public class User {
 	@Column(nullable = false, unique = true)
 	@JsonProperty
 	private String username;
-	
+	@JsonProperty(access = Access.WRITE_ONLY )
+	@Column(nullable = false)
+	private String password;
+
+	@Column(nullable = false)
+	private String salt;
+
+	public User(String username, String password) {
+		this.username = username;
+		this.password = password;
+	}
+
+	public User() {
+	}
+
+	public String getSalt() {
+		return salt;
+	}
+
+	public void setSalt(String salt) {
+		this.salt = salt;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
 	@OneToOne(cascade = CascadeType.ALL)
     @JoinColumn(name = "cart_id", referencedColumnName = "id")
 	@JsonIgnore
